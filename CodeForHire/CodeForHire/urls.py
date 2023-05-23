6d0a3133b70cb8c42ae9c8cec5556beb0576ee2d
Georgi Popov <84380232+g-popov16@users.noreply.github.com>
2023-05-23 20:57:12 +0300

    settings

--- CodeForHire/CodeForHire/urls.py
+++ CodeForHire/CodeForHire/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from core.views import base
@@ -21,4 +23,4 @@ from core.views import base
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', base, name='base'),
-]
+] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
