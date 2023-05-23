b4ba3b5b547b15946d77fd49534c1cfd39aac38d
Georgi Popov <84380232+g-popov16@users.noreply.github.com>
2023-05-23 23:53:52 +0300

    added sign up template

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from item.models import Item, Category
+from .forms import SignupForm
 
 
 def index(request):
@@ -14,3 +15,11 @@ def index(request):
 
 def contact(request):
      return render(request, 'core/contact.html')
+
+def signup(request):
+
+     form = SignupForm()
+
+     return render(request, 'core/signup.html',{
+                        'form': form,
+                   })
--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -18,10 +18,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from core.views import index, contact
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
 
 urlpatterns = [
     path('', include('core.urls')),
     path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
