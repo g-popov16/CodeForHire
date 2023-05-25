285f1245a020b7e05ae257c6b336c38edf3393bd
Georgi Popov <84380232+g-popov16@users.noreply.github.com>
2023-05-25 23:09:10 +0300

    delete and edit and browse

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
+
+def items(request):
+    items = Item.objects.all()
+
+    return render(request, 'item/items.html', {
+        'items': items,
+    })
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -28,9 +35,34 @@ def new(request):
     else:
         form = NewItemForm()
 
-    form = NewItemForm()
 
     return render(request, 'item/form.html', {
         'form': form,
         'title': 'New Item',
     })
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+        
+    else:
+        form = EditItemForm(instance=item)
+
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit Item',
+    })
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+    return redirect('dashboard:index')
--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -22,14 +22,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
-
-
 urlpatterns = [
     path('', include('core.urls')),
     path('items/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
